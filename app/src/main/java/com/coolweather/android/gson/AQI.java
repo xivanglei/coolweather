84fa334234cfecb3fc058731e1ac7b1e7691603a
xianglei <xianglei19881119@sina.com>
2017-12-15 19:24:22 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by xianglei on 2017/12/14.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
